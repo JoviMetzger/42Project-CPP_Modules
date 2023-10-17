d13366b89b3d26f116992ee03479f2c4643110f6
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-10-17 12:03:41 +0200

    Update main.cpp

--- Module 01/ex02/main.cpp
+++ Module 01/ex02/main.cpp
@@ -5,17 +5,34 @@
 # define L_BLUE "\e[1;36m"
 # define RESET "\033[0m"
 
-void main()
+int	main()
 {
-    std::string str = "HI THIS IS BRAIN";
-    // stringPTR:
-    // stringREF:
+	std::cout << "\n ---------- Subject requirements ----------\n" << std::endl;
+	// A string variable
+	std::string	str = "HI THIS IS BRAIN";
 
-    std::cout << L_BLUE << "The memory address of the string variable" << RESET << std::endl;
-    std::cout << L_BLUE << "The memory address held by stringPTR" << RESET << std::endl;
-    std::cout << L_BLUE << "The memory address held by stringREF" << RESET << std::endl;
+	// A pointer to the string
+	std::string*	stringPTR = &str;
 
-    std::cout << L_BLUE << "The value of the string variable" << RESET << std::endl;
-    std::cout << L_BLUE << "The value pointed to by stringPTR" << RESET << std::endl;
-    std::cout << L_BLUE << "The value pointed to by stringREF" << RESET << std::endl;
+	// A reference to the string
+	std::string&	stringREF = str;
+
+	// Print the memory addresses
+	std::cout << L_BLUE << "The memory address of the string variable: " << RESET << &str << std::endl;
+	std::cout << L_BLUE << "The memory address held by stringPTR: " << RESET << stringPTR << std::endl;
+	std::cout << L_BLUE << "The memory address held by stringREF: " << RESET << &stringREF << std::endl;
+
+	// Print the values
+	std::cout << L_BLUE << "The value of the string variable: " << RESET << str << std::endl;
+	std::cout << L_BLUE << "The value pointed to by stringPTR: " << RESET << *stringPTR << std::endl;
+	std::cout << L_BLUE << "The value pointed to by stringREF: " << RESET << stringREF << std::endl;
+
+	std::cout << "\n\n ---------- Extra Test ----------\n" << std::endl;
+
+	int	x = 10;
+	int	y = 20;
+
+	// Display initial values
+	std::cout << L_BLUE << "Initial values:" << RESET << std::endl;
+	std::cout << "x: " << x << "\ny: " << y << std::endl;
 }
