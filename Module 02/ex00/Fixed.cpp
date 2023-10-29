1a89051bba99c14c5a532cdd0dfdeb0f4c1d8f0a
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-10-29 11:23:29 +0100

    Update Fixed.cpp

--- Module 02/ex00/Fixed.cpp
+++ Module 02/ex00/Fixed.cpp
@@ -7,7 +7,7 @@ Fixed::Fixed()
 	std::cout << "Default constructor called" << std::endl;
 }
 
-// Destructor
+// Destructor - Used to perform cleanup operations
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
