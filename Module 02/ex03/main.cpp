46129ad32873dbf20853180a992eebafd7d0e701
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-11-01 10:24:30 +0100

    Update main.cpp

--- Module 02/ex03/main.cpp
+++ Module 02/ex03/main.cpp
@@ -10,11 +10,11 @@ int	main()
 	const	Point B(5, 0);
 	const	Point C(2.5f, 5);
 
-	Point	a(4, 2.3f);
-	Point	b(4.99f, 1);
-	Point	c(4.0f, 4.0f);
-	Point	d(6, 6);
-	Point	e(2, 2);
+	Point	a(0, 0);
+	Point	b(6, 6);
+	Point	c(2, 2);
+	Point	d(3.2f, 1.0f);
+	Point	e(-2, 2);
 
 	std::cout << BLUE << "a: " << RESET << (bsp(A, B, C, a) ? "Inside" : "Outside") << std::endl;
 	std::cout << BLUE << "b: " << RESET << (bsp(A, B, C, b) ? "Inside" : "Outside") << std::endl;
