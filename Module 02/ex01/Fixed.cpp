511940b3756d472f6595a9372e9c577948042bbc
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-11-01 10:42:04 +0100

    Update Fixed.cpp

--- Module 02/ex01/Fixed.cpp
+++ Module 02/ex01/Fixed.cpp
@@ -18,7 +18,7 @@ Fixed::~Fixed()
 Fixed::Fixed(const Fixed& value)
 {
 	std::cout << "Copy constructor called" << std::endl; 
-	_fixedPoint = value._fixedPoint; 
+	*this = value;
 }
 
 // Constructor - Converts int to fixed-point value
