e45343d10472bbb0032234a8980bcce147f452dd
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-11-05 14:26:23 +0100

    Add files via upload

--- Module 03/ex00/ClapTrap.cpp
+++ Module 03/ex00/ClapTrap.cpp
@@ -9,14 +9,14 @@ ClapTrap::ClapTrap()
 // Constructor
 ClapTrap::ClapTrap(std::string name): _damage(0)
 {
-    _name = name;
-    _hitPoints = 10;
-    _energyPoints = 10;
-    //_damage = 0;
+	_name = name;
+	_hitPoints = 10;
+	_energyPoints = 10;
+	//_damage = 0;
 	std::cout << "Player " << YELLOW << _name << RESET << " starts with:" << std::endl;
-    std::cout << "[" << _hitPoints << "] HitPoints," <<std::endl;
-    std::cout << "[" << _energyPoints << "] EnergyPoints," << std::endl;
-    std::cout << "[ " << _damage << "] Damage." << std::endl << std::endl;
+	std::cout << "[" << _hitPoints << "] HitPoints," <<std::endl;
+	std::cout << "[" << _energyPoints << "] EnergyPoints," << std::endl;
+	std::cout << "[ " << _damage << "] Damage." << std::endl << std::endl;
 }
 
 // Destructor
@@ -28,64 +28,80 @@ ClapTrap::~ClapTrap()
 // Copy constructor - Used to initialize a new object
 ClapTrap::ClapTrap(const ClapTrap& value)
 {
-	std::cout << "Copy constructor called" << std::endl; //RM 
 	*this = value;
 }
 
 // Copy assignment operator - Used to make a deep copy of one object
 ClapTrap	&ClapTrap::operator=(const ClapTrap& value)
 {
-	std::cout << "Copy assignment operator called " << std::endl; //RM
 	// Perform a deep copy
-    _name = value._name;
-    _hitPoints = value._hitPoints;
-    _energyPoints = value._energyPoints;
-    _damage = value._damage;
+	_name = value._name;
+	_hitPoints = value._hitPoints;
+	_energyPoints = value._energyPoints;
+	_damage = value._damage;
 	return *this;
 }
 
-void ClapTrap::attack(const std::string& target)
+void	ClapTrap::attack(const std::string& target)
 {
-    if (_hitPoints > 0 && _energyPoints > 0)
-    {
-        std::cout << "🤖 ClapTrap " << _name << PURPLE " attacks " << RESET 
-        << target << ", cousing " << _damage << " points of damage!" << std::endl;
-        _energyPoints -= 1;
-    }
-    else
-    {
-        std::cout << "❌ ClapTrap " << _name << PURPLE << " can't attack." 
-        << RESET << " No hit points or energy points left!" << std::endl;
-    }
+	if (_hitPoints == 0)
+	{
+		std::cout << "❌ ClapTrap " << _name << PURPLE << " can't attack." 
+		<< RESET << " No hit points left!" << std::endl;
+	}
+	else if (_energyPoints == 0)
+	{
+		std::cout << "❌ ClapTrap " << _name << PURPLE << " can't attack." 
+		<< RESET << " No energy points left!" << std::endl;
+	}
+	else
+	{
+		std::cout << "🤖 ClapTrap " << _name << PURPLE " attacks " << RESET 
+		<< target << ", cousing " << _damage << " points of damage!" << std::endl;
+		_energyPoints -= 1;
+	}
 }
 
-void ClapTrap::takeDamage(unsigned int amount)
+void	ClapTrap::takeDamage(unsigned int amount)
 {
-    if (_hitPoints > 0)
-    {
-        _hitPoints -= amount;
-        std::cout << "👊 ClapTrap " << _name << " takes " << amount 
-        << " points of " << PURPLE "damage" << RESET << "!" << std::endl;
-    }
-    else
-    {
-        std::cout << "❌ ClapTrap " << _name << PURPLE << " can't take damage." 
-        << RESET << " No hit points left!" << std::endl;
-    }
+	if (amount > _hitPoints)
+	{
+		_hitPoints = 0;
+		std::cout << "❌ ClapTrap " << _name << PURPLE << " can't take damage." 
+		<< RESET << " No hit points left!" << std::endl;
+	}
+	else if(_hitPoints > 0)
+	{
+		_hitPoints -= amount;
+		if (_hitPoints < 0) 
+			_hitPoints = 0;
+		std::cout << "👊 ClapTrap " << _name << " takes " << amount 
+		<< " points of " << PURPLE "damage" << RESET << "!" << std::endl;
+	}
 }
 
-void ClapTrap::beRepaired(unsigned int amount)
+void	ClapTrap::beRepaired(unsigned int amount)
 {
-    if (_hitPoints > 0 && _energyPoints > 0)
-    {
-        _hitPoints += amount;
-        _energyPoints -= 1;
-        std::cout << "♻️  ClapTrap " << _name << " is " << PURPLE << "repaired" 
-        << RESET << " for " << amount << " hit points!" << std::endl;
-    }
-    else
-    {
-        std::cout << "❌ ClapTrap " << _name << PURPLE << " can't be repaired." 
-        << RESET << " No hit points or energy points left!" << std::endl;
-    }
+	if (_hitPoints <= 0)
+	{
+		std::cout << "❌ ClapTrap " << _name << PURPLE << " can't be repaired." 
+		<< RESET << " No hit points left!" << std::endl;
+	}
+	else if (_energyPoints <= 0)
+	{
+		std::cout << "❌ ClapTrap " << _name << PURPLE << " can't be repaired." 
+		<< RESET << " No energy points left!" << std::endl;
+	}
+	else if (_hitPoints + amount > 10)
+	{
+		std::cout << "❌ ClapTrap " << _name << PURPLE << " can't be repaired." 
+		<< RESET << " Reaching over maximum hit points!" << std::endl;
+	}
+	else
+	{
+		_hitPoints += amount;
+		_energyPoints -= 1;
+		std::cout << "♻️  ClapTrap " << _name << " is " << PURPLE << "repaired" 
+		<< RESET << " for " << amount << " hit points!" << std::endl;
+	}
 }
--- Module 03/ex00/main.cpp
+++ Module 03/ex00/main.cpp
@@ -1,18 +1,35 @@
 #include "ClapTrap.hpp"
 
-int main()
+int	main()
 {
-    std::string ClapTrapName;
+	std::string	ClapTrapName;
 
-    std::cout << GREEN << " - Please choose a name for your player -" 
-    << RESET << std::endl;
-    std::cout << GREEN << ">> " << RESET;
-    getline(std::cin, ClapTrapName);
+	std::cout << GREEN << " - Please choose a name for your player -" 
+	<< RESET << std::endl;
+	std::cout << GREEN << ">> " << RESET;
+	getline(std::cin, ClapTrapName);
 
-    ClapTrap ClapTrap(ClapTrapName);    //Create a ClapTrap object
-    ClapTrap.attack("CL4P-TP");
-    ClapTrap.takeDamage(5);
-    ClapTrap.beRepaired(3);
-    
-    return (0);
+	ClapTrap	claptrap(ClapTrapName);	// Create a ClapTrap object
+	std::cout << std::endl;
+
+	claptrap.attack("CL4P-42");
+	claptrap.takeDamage(5);
+	claptrap.beRepaired(3);
+
+	// Test: repairing with an amount over the max (10)
+	claptrap.beRepaired(12);		// ClapTrap can't be repaired
+
+	// Test: taking more hit points
+	claptrap.takeDamage(8);			// ClapTrap had 8 hit points left
+						// Now has 0 hit points left
+
+	// Test: attacking when no hit points or energy points are left
+	claptrap.takeDamage(2);			// ClapTrap can't take damage
+	claptrap.attack("CL4P-42");		// ClapTrap can't attack
+
+	// Test: repairing when no hit points or energy points are left
+	claptrap.beRepaired(3);			// ClapTrap can't be repaired
+
+	std::cout << std::endl;
+	return (0);
 }
