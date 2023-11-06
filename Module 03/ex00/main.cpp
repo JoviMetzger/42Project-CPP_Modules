e8f682f9cba7385038733b2188e0eb3f4f0170cb
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-11-06 15:47:03 +0100

    Update main.cpp

--- Module 03/ex00/main.cpp
+++ Module 03/ex00/main.cpp
@@ -9,26 +9,27 @@ int	main()
 	std::cout << GREEN << ">> " << RESET;
 	getline(std::cin, ClapTrapName);
 
-	ClapTrap	claptrap(ClapTrapName);	// Create a ClapTrap object
+	ClapTrap	clapA;			// Create a ClapTrap object
+	ClapTrap	clapB(ClapTrapName);
 	std::cout << std::endl;
 
-	claptrap.attack("CL4P-42");
-	claptrap.takeDamage(5);
-	claptrap.beRepaired(3);
+	clapB.attack("CL4P-42");
+	clapB.takeDamage(5);
+	clapB.beRepaired(3);
 
 	// Test: repairing with an amount over the max (10)
-	claptrap.beRepaired(12);		// ClapTrap can't be repaired
+	clapB.beRepaired(12);			// ClapTrap can't be repaired
 
 	// Test: taking more hit points
-	claptrap.takeDamage(8);			// ClapTrap had 8 hit points left
+	clapB.takeDamage(8);			// ClapTrap had 8 hit points left
 						// Now has 0 hit points left
 
 	// Test: attacking when no hit points or energy points are left
-	claptrap.takeDamage(2);			// ClapTrap can't take damage
-	claptrap.attack("CL4P-42");		// ClapTrap can't attack
+	clapB.takeDamage(2);			// ClapTrap can't take damage
+	clapB.attack("CL4P-42");		// ClapTrap can't attack
 
 	// Test: repairing when no hit points or energy points are left
-	claptrap.beRepaired(3);			// ClapTrap can't be repaired
+	clapB.beRepaired(3);			// ClapTrap can't be repaired
 
 	std::cout << std::endl;
 	return (0);
