2e837fdf0f8a6ce80ab990d468a38c53f08e2a19
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-12-22 23:08:45 +0100

    Update ShrubberyCreationForm.cpp

--- Module 05/ex03/src/ShrubberyCreationForm.cpp
+++ Module 05/ex03/src/ShrubberyCreationForm.cpp
@@ -54,17 +54,17 @@ void ShrubberyCreationForm::execute(Bureaucrat const &executer) const
 	{
 		// Execute the action specific to this form
 		// Create a file <_target>_shrubbery in the working directory
-		 std::ofstream fileName;
-		 std::string	output_filename = std::string(_target + "_shrubbery");
-		 const char*	conv_outfilename = output_filename.c_str();		// Converts to const char*
+		std::ofstream fileName;
+		std::string	output_filename = std::string(_target + "_shrubbery");
+		const char*	conv_outfilename = output_filename.c_str();		// Converts to const char*
 
-	    fileName.open(conv_outfilename);
-	    if (!fileName)
-	    {
-		    std::cout << "Coulnd't open the output file" << std::endl;
-		    return ;
-	    }
-	    fileName << ASCII_TREE;
-	    fileName.close();
+		fileName.open(conv_outfilename);
+	    	if (!fileName)
+	    	{
+		   	 std::cout << "Coulnd't open the output file" << std::endl;
+		   	 return ;
+	    	}
+	    	fileName << ASCII_TREE;
+	    	fileName.close();
 	}
 }
