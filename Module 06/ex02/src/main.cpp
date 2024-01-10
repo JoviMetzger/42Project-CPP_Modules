b938912515be046bd0a4385b7436827cf474294b
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-01-10 15:52:05 +0100

    Update main.cpp

--- Module 06/ex02/src/main.cpp
+++ Module 06/ex02/src/main.cpp
@@ -8,24 +8,23 @@
 
 int	main(void)
 {
-    // prints the actual type of the object
-    std::cout << BLUE << "Testing identify(Base* p)" << RESET << std::endl;
+	// prints the actual type of the object
+	std::cout << BLUE << "Testing identify(Base* p)" << RESET << std::endl;
 	{
-        Base* obj = generate();
-        identify(obj);
+		Base* obj = generate();
+		identify(obj);
 
-        delete obj;
-        std::cout << std::endl;
-    }
-    // prints the actual type of the object
-    std::cout << BLUE << "Testing identify(Base& p)" << RESET << std::endl;
-    {
-        Base* obj2 = generate();
-        identify(*obj2);
+		delete obj;
+		std::cout << std::endl;
+	}
+	// prints the actual type of the object
+	std::cout << BLUE << "Testing identify(Base& p)" << RESET << std::endl;
+	{
+		Base* obj2 = generate();
+		identify(*obj2);
 
-        delete obj2;
-        std::cout << std::endl;
-    }
-
+		delete obj2;
+		std::cout << std::endl;
+	}
 	return (0);
 } 
