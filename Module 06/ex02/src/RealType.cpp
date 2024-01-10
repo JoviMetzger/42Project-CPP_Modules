dbe0f415ef500f8de5e2574e16e8c51577b4f286
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-01-10 12:58:15 +0100

    Update RealType.cpp

--- Module 06/ex02/src/RealType.cpp
+++ Module 06/ex02/src/RealType.cpp
@@ -1,35 +1,43 @@
 #include "../header/RealType.hpp"
 
-// Default Constructor
-Realtype::Realtype()
-{
-	std::cout << "Default constructor called" << std::endl;
-	_name = "Default Name";
-}
-
 // Destructor
-Realtype::~Realtype()
+Base::~Base()
 {
 	std::cout << "Destructor called" << std::endl;
 }
 
-// Copy constructor
-Realtype::Realtype(const Realtype& value)
+// It randomly instanciates A, B or C and returns the instance as a Base pointer.
+Base *generate(void)
 {
-	std::cout << "Copy constructor called" << std::endl; 
-	*this = value;
-}
+	srand(static_cast<unsigned int>(time(NULL)));
+	int random_num = rand() % 3;
 
-// Copy assignment operator
-Realtype	&Realtype::operator=(const Realtype& value)
-{
-	std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &value)	
+	switch (random_num)
 	{
-		// Perform a deep copy
-		_name = value._name;
+		case 0:
+			return (new A_Class());
+		case 1:
+			return (new B_Class());
+		case 2:
+			return (new C_Class());
+		default:
+			return (NULL);
 	}
-	return *this;
 }
 
-// Member Function
+// It prints the actual type of the object pointed to by p: "A", "B" or "C".
+void identify(Base* p)
+{
+	if (dynamic_cast<A_Class*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B_Class*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C_Class*>(p))
+		std::cout << "C" << std::endl;
+}
+
+// It prints the actual type of the object pointed to by p: "A", "B" or "C".
+void identify(Base& p)
+{
+	identify(&p); // Calls the pointer version
+}
