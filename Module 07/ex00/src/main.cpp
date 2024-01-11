53d25960e1df7cfc124b12e2e16385c6310f3f1c
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-01-11 19:33:10 +0100

    Add files via upload

---
+++ Module 07/ex00/src/main.cpp
@@ -0,0 +1,75 @@
+#include "../header/whatever.hpp"
+
+// ---------- Swap() ----------
+template <typename T> 
+void swap(T &a, T &b)	// Need to be the refrence, because you are returning nothing (void): any changes made to 'a' and 'b' inside the swap() will affect the original variables passed to the function. 
+{
+	T swp;
+
+	swp = a;
+	a = b;
+	b = swp;
+}
+
+// ---------- Min() -----------
+template <typename T> 
+T min(T a, T b)
+{
+	if (a > b)
+		return (b);
+	else if (a < b)
+		return (a);
+	else
+	{
+		std::cout << "Equal";
+		return (0);
+	}
+		
+}
+
+// ---------- Max() -----------
+template <typename T> 
+T max(T a, T b)
+{
+	if (a > b)
+		return (a);
+	else if (a < b)
+		return (b);
+	else
+	{
+		std::cout << "Equal";
+		return (0);
+	}
+}
+
+// ---------- Main() ----------
+int main( void ) 
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << std::endl;
+
+	char j = 'w';
+	char k = 'w';
+
+	::swap(j, k);
+	std::cout << "j = " << j << ", k = " << k << std::endl;
+	std::cout << "min( j, k ) = " << ::min( j, k ) << std::endl;
+	std::cout << "max( j, k ) = " << ::max( j, k ) << std::endl;
+
+	return 0;
+}
---
+++ Module 07/ex01/src/main.cpp
@@ -0,0 +1,129 @@
+#include "../header/iter.hpp"
+
+// COLORS
+# define BLUE "\e[1;34m"
+# define RESET "\033[0m"
+
+// ---------- iter() ----------
+// array: Address of an array
+// lenght: Length of the array
+// func: Function that will be call on every element of the array
+template <typename T, typename size_t, typename Function>
+void	iter(T *array, size_t lenght, Function func)
+{
+	for (size_t i = 0; i < lenght; ++i)
+		func(array[i]);
+}
+
+// ---------- printElement() ----------
+// Example func 1: Function to be used with iter
+template <typename T>
+void	printElement(T& element) 
+{
+	std::cout << element << ", ";
+}
+
+// ---------- turnIntoA() ----------
+// Example func 2: Function to be used with iter
+template <typename T>
+void	turnIntoA(T& element) 
+{
+	element = 'a';
+}
+
+// ---------- Main() ----------
+int	main() 
+{
+	std::cout << BLUE << " ----- Testing func 1: printElement -----" << RESET << std::endl;
+	std::cout << BLUE << " Testing intArray" << RESET << std::endl;
+	// Example 1: Using iter with an array of int - with function printElement()
+	{
+		int intArray[] = {1, 2, 3, 4, 5};
+		size_t intArrayLength = sizeof(intArray) / sizeof(intArray[0]);
+
+		std::cout << "Applying iter to intArray: ";
+		iter(intArray, intArrayLength, printElement<int>);
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << BLUE << " Testing doubleArray" << RESET << std::endl;
+    // Example 2: Using iter with an array of doubles - with function printElement()
+	{
+		double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+		size_t doubleArrayLength = sizeof(doubleArray) / sizeof(doubleArray[0]);
+
+		std::cout << "Applying iter to doubleArray: ";
+		iter(doubleArray, doubleArrayLength, printElement<double>);
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << BLUE << " Testing charArray " << RESET << std::endl;
+	// Example 3: Using iter with an array of char - with function printElement()
+	{
+		char charArray[] = {'x', 'y', 'z', '1', '2'};
+		size_t charArrayLength = sizeof(charArray) / sizeof(charArray[0]);
+
+		std::cout << "Applying iter to charArray: ";
+		// Applying iter to turn everything into 'a'
+		iter(charArray, charArrayLength, printElement<char>);
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << BLUE << " ----- Testing func 2: turnIntoA -----" << RESET << std::endl;
+	std::cout << BLUE << " Testing intArray" << RESET << std::endl;
+	// Example 4: Using iter with an array of int - with function turnIntoA()
+	{
+		int intArray[] = {1, 2, 3, 4, 5};
+		size_t intArrayLength = sizeof(intArray) / sizeof(intArray[0]);
+
+		std::cout << "Original intArray: ";
+		for (size_t i = 0; i < intArrayLength; ++i) 
+			std::cout << intArray[i] << ", ";
+		std::cout << std::endl;
+
+		// Applying iter to turn everything into 'a'
+		iter(intArray, intArrayLength, turnIntoA<int>);
+
+		std::cout << "After applying iter: ";
+		for (size_t i = 0; i < intArrayLength; ++i) 
+			std::cout << intArray[i] << ", ";
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << BLUE << " Testing doubleArray" << RESET << std::endl;
+	// Example 5: Using iter with an array of doubles- with function turnIntoA()
+	{
+		double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+		size_t doubleArrayLength = sizeof(doubleArray) / sizeof(doubleArray[0]);
+
+		std::cout << "Original doubleArray: ";
+		for (size_t i = 0; i < doubleArrayLength; ++i) 
+			std::cout << doubleArray[i] << ", ";
+		std::cout << std::endl;
+
+		// Applying iter to turn everything into 'a'
+		iter(doubleArray, doubleArrayLength, turnIntoA<double>);
+
+		std::cout << "After applying iter: ";
+		for (size_t i = 0; i < doubleArrayLength; ++i) 
+			std::cout << doubleArray[i] << ", ";
+		std::cout << std::endl << std::endl;
+	}
+	std::cout << BLUE << " Testing charArray " << RESET << std::endl;
+	// Example 6: Using iter with an array of char - with function turnIntoA()
+	{
+		char charArray[] = {'x', 'y', 'z', '1', '2'};
+		size_t charArrayLength = sizeof(charArray) / sizeof(charArray[0]);
+
+		std::cout << "Original charArray: ";
+		for (size_t i = 0; i < charArrayLength; ++i) 
+			std::cout << charArray[i] << ", ";
+		std::cout << std::endl;
+
+		// Applying iter to turn everything into 'a'
+		iter(charArray, charArrayLength, turnIntoA<char>);
+
+		std::cout << "After applying iter: ";
+		for (size_t i = 0; i < charArrayLength; ++i) 
+			std::cout << charArray[i] << ", ";
+		std::cout << std::endl;
+	}
+
+    return 0;
+}
