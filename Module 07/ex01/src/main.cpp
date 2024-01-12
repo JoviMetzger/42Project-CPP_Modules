31a237a58bf76f672f26e3adebda2866ad3c5af1
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-01-12 12:08:35 +0100

    Update main.cpp

--- Module 07/ex01/src/main.cpp
+++ Module 07/ex01/src/main.cpp
@@ -4,33 +4,6 @@
 # define BLUE "\e[1;34m"
 # define RESET "\033[0m"
 
-// ---------- iter() ----------
-// array: Address of an array
-// lenght: Length of the array
-// func: Function that will be call on every element of the array
-template <typename T, typename size_t, typename Function>
-void	iter(T *array, size_t lenght, Function func)
-{
-	for (size_t i = 0; i < lenght; ++i)
-		func(array[i]);
-}
-
-// ---------- printElement() ----------
-// Example func 1: Function to be used with iter
-template <typename T>
-void	printElement(T& element) 
-{
-	std::cout << element << ", ";
-}
-
-// ---------- turnIntoA() ----------
-// Example func 2: Function to be used with iter
-template <typename T>
-void	turnIntoA(T& element) 
-{
-	element = 'a';
-}
-
 // ---------- Main() ----------
 int	main() 
 {
