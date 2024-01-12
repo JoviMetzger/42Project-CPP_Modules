2d564f5e69747bcf2c417632d046c8ebf994b724
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-01-12 12:07:56 +0100

    Update main.cpp

--- Module 07/ex00/src/main.cpp
+++ Module 07/ex00/src/main.cpp
@@ -1,46 +1,5 @@
 #include "../header/whatever.hpp"
 
-// ---------- Swap() ----------
-template <typename T> 
-void swap(T &a, T &b)	// Need to be the refrence, because you are returning nothing (void): any changes made to 'a' and 'b' inside the swap() will affect the original variables passed to the function. 
-{
-	T swp;
-
-	swp = a;
-	a = b;
-	b = swp;
-}
-
-// ---------- Min() -----------
-template <typename T> 
-T min(T a, T b)
-{
-	if (a > b)
-		return (b);
-	else if (a < b)
-		return (a);
-	else
-	{
-		std::cout << "Equal";
-		return (0);
-	}	
-}
-
-// ---------- Max() -----------
-template <typename T> 
-T max(T a, T b)
-{
-	if (a > b)
-		return (a);
-	else if (a < b)
-		return (b);
-	else
-	{
-		std::cout << "Equal";
-		return (0);
-	}
-}
-
 // ---------- Main() ----------
 int main( void ) 
 {
