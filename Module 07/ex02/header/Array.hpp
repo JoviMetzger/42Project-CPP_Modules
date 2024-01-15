0abf7da5a2292275ad27cb12c2566570571d1b40
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-01-15 13:05:52 +0100

    Add files via upload

--- Module 07/ex02/header/Array.hpp
+++ Module 07/ex02/header/Array.hpp
@@ -2,5 +2,79 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# include <cstdlib>
+# include <string>
+
+template <typename T>
+class	Array
+{
+	private:
+		T* _elements;
+		unsigned int _arraySize; 
+	public:
+		// (1): Default Constructor - with no parameter: Creates an empty array
+		Array() : _elements(NULL), _arraySize(0)
+		{
+			// std::cout << "Default constructor called" << std::endl;
+		}
+
+		// Destructor
+		~Array()
+		{
+			// std::cout << "Destructor called" << std::endl;
+			delete[] _elements;
+		}
+
+		// (2): Constructor - Construction with an unsigned int n as a parameter
+		Array(unsigned int n)
+		{
+			_elements = new T[n];
+			_arraySize = n;
+		}
+
+		// (3): Copy constructor 
+		Array(const Array& value)
+		{
+			std::cout << "Copy constructor called" << std::endl;
+			_arraySize = value._arraySize;
+			_elements = new T[_arraySize];
+
+			for (unsigned int i = 0; i < _arraySize; i++)
+				_elements[i] = value._elements[i];
+		}
+
+		// (4): Assignment= operator
+		Array &operator=(const Array& value)
+		{
+			std::cout << "Assignment operator called" << std::endl;
+			if (this != &value)
+			{
+				delete[] _elements; // Release existing memory
+				_arraySize = value._arraySize;
+				_elements = new T[_arraySize];
+
+				for (unsigned int i = 0; i < value._arraySize; i++)
+					_elements[i] = value._elements[i];
+			}
+			return *this;
+		}
+
+		// (5): Subscript[] operator
+		T& operator[](unsigned int index)
+		{
+			if (index >= _arraySize)
+				throw std::out_of_range("Index out of bounds");
+			return _elements[index];
+		}
+
+		// (6): Member function size()
+		unsigned int size() const
+		{
+			return _arraySize;
+		}
+};
+
+
+
 
 #endif
--- Module 07/ex02/src/main.cpp
+++ Module 07/ex02/src/main.cpp
@@ -4,64 +4,59 @@
 # define BLUE "\e[1;34m"
 # define RESET "\033[0m"
 
-// // --------- Own main ----------
-// int	main(void)
-// {
-// 	return (0);
-// } 
+int	main()
+{
+	std::cout << BLUE << "Testing different the constructors" << RESET << std::endl;
+	try {
+		// Test cases
+		Array<int> arr1; // Construction with no parameter, creates an empty array
+		std::cout << "arr1 size: " << arr1.size() << std::endl;
 
+		Array<int> arr2(5); // Constructor, creates an array of 5 elements
+		std::cout << "arr2 size: " << arr2.size() << std::endl;
 
-// // --------- main given by subject ----------
-// #include <iostream>
-// #include <Array.hpp>
+		arr2[3] = 42; // Accessing elements through subscript operator
+		std::cout << "arr2[3]: " << arr2[3] << std::endl;
+		std::cout << std::endl;
 
-// #define MAX_VAL 750
-// int main(int, char**)
-// {
-//     Array<int> numbers(MAX_VAL);
-//     int* mirror = new int[MAX_VAL];
-//     srand(time(NULL));
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         const int value = rand();
-//         numbers[i] = value;
-//         mirror[i] = value;
-//     }
-//     //SCOPE
-//     {
-//         Array<int> tmp = numbers;
-//         Array<int> test(tmp);
-//     }
+		Array<int> arr3 = arr2; // Copy constructor
+		std::cout << "arr3 size: " << arr3.size() << std::endl;
+		std::cout << "arr3[3]: " << arr3[3] << std::endl;
+		std::cout << std::endl;
 
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         if (mirror[i] != numbers[i])
-//         {
-//             std::cerr << "didn't save the same value!!" << std::endl;
-//             return 1;
-//         }
-//     }
-//     try
-//     {
-//         numbers[-2] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
-//     try
-//     {
-//         numbers[MAX_VAL] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
+		Array<int> arr4;
+		arr4 = arr2; // Assignment operator
+		std::cout << "arr4 size: " << arr4.size() << std::endl;
+		std::cout << "arr4[3]: " << arr4[3] << std::endl;
+		std::cout << std::endl;
 
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         numbers[i] = rand();
-//     }
-//     delete [] mirror;//
-//     return 0;
-// }
+		// Test: Subscript operator with out-of-bounds index
+		std::cout << arr2[10] << std::endl;
+	} catch (const std::exception& e) {
+        	std::cerr << "Exception caught: " << e.what() << std::endl;
+    	}
+
+	std::cout << BLUE << "\nTesting different arrays (int, double, char)" << RESET << std::endl;
+	try {
+		// Test case <int>
+		Array<int> arr1;	// Construction with no parameter, creates an empty array
+		std::cout << "arr1 size: " << arr1.size() << std::endl;
+
+        	// Test case <doule>>
+        	Array<double> arr2(5); // Constructor, creates an array of 5 elements
+		arr2[1] = 2.2;
+        	std::cout << "arr2 size: " << arr2.size() << std::endl;
+		std::cout << "arr2[1]: " << arr2[1] << std::endl;
+		std::cout << std::endl;
+
+        	// Test case <char>
+       		Array<char> arr4(3);
+       		arr4[1] = 'X';
+		std::cout << "arr4 size: " << arr4.size() << std::endl;
+        	std::cout << "arr4[1]: " << arr4[1] << std::endl;
+
+    	} catch (const std::exception& e) {
+        	std::cerr << "Exception caught: " << e.what() << std::endl;
+    	}
+	return 0;
+}
