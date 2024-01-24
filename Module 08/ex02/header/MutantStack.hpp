44767fe2509045530dfec33e8095edd29c2f62ca
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-01-24 11:57:56 +0100

    Add files via upload

--- Module 08/ex02/header/MutantStack.hpp
+++ Module 08/ex02/header/MutantStack.hpp
@@ -2,12 +2,59 @@
 # define MUTANTSTACK_HPP
 
 # include <iostream>
+# include <algorithm>
+# include <stack>
 
-class	Array
+template <class T, class Container= std::deque <T>>
+class	MutantStack : public std::stack<T>
 {
 	private:
 	public:
-		
+		// Default Constructor
+		MutantStack() : std::stack<T>() {};
+
+		// Destructor
+		~MutantStack() {};
+
+		// Copy constructor
+		MutantStack(const MutantStack& value)
+		{
+			// std::cout << "Copy constructor called" << std::endl;
+			*this = value;
+		}
+
+		// Assignment= operator
+		MutantStack<T> &operator=(const MutantStack<T>& value)
+		{
+			// std::cout << "Assignment operator called" << std::endl;
+			if (this != &value)
+			{
+				this->c = value.c;
+			}
+			return *this;
+		}
+
+		// Constructor
+		MutantStack(const std::stack<T> &value) : std::stack<T>(value) {};
+	
+		// Member function
+		/* Alias for iterator
+		 * 	- Type alias iterator for the iterator type of the underlying container of std::stack<T>. 
+		 *	- This is using the typename keyword to specify that container_type::iterator is a type. 
+		 *	- 'container_type' A type that provides the base container to be adapted by a stack. */
+		using iterator = typename std::stack<T>::container_type::iterator;
+
+		// Iterator functions
+		// 'this->c' refers to the underlying container of the inherited 'std::stack<T>'
+		iterator begin()
+		{
+			return (this->c.begin());
+		}
+
+		iterator end()
+		{
+			return (this->c.end());
+		}
 };
 
 #endif
--- Module 08/ex02/src/main.cpp
+++ Module 08/ex02/src/main.cpp
@@ -4,7 +4,105 @@
 # define BLUE "\e[1;34m"
 # define RESET "\033[0m"
 
-int	main()
+int main()
 {
+	std::cout << BLUE << "main() from subject" << RESET << std::endl;
+	{
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl << std::endl; // Printing the top element
+		mstack.pop();
+		std::cout << mstack.size() << std::endl << std::endl; // After popping an element and printing the size
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+
+		// Iterating over MutantStack and printing each element
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+
+		// Testing with std::stack
+		std::stack<int> s(mstack);
+		std::cout << std::endl;
+	}
+	std::cout << BLUE << "Testing the iterators" << RESET << std::endl;
+	{
+		MutantStack<int> mstack;
+
+		mstack.push(1);
+		mstack.push(2);
+		mstack.push(3);
+		mstack.push(4);
+		mstack.push(5);
+
+		std::cout << mstack.size() << std::endl << std::endl; // Printing the size
+
+		// Iterating over MutantStack and printing each element
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::cout << std::endl;
+
+		// Printing the top element
+		MutantStack<int>::iterator it_begin = mstack.begin();
+		std::cout << "original top: " << *it_begin << std::endl;
+		
+		// Test if ++ work with iterator - printing the top element +1
+		++it_begin;
+		std::cout << "++ top: " << *it_begin << std::endl;
+
+		// Test if -- work with iterator - printing the top element -1
+		--it_begin;
+		std::cout << "-- top: " << *it_begin << std::endl;
+	}
+	std::cout << BLUE << "\nTesting strings" << RESET << std::endl;
+	{
+		MutantStack<std::string> mstack;
+
+		mstack.push("Gustav");
+		mstack.push("Steve");
+		mstack.push("Bob");
+	
+		std::cout << mstack.size() << std::endl << std::endl; // Printing the size
+
+		// Iterating over MutantStack and printing each element
+		MutantStack<std::string>::iterator it = mstack.begin();
+		MutantStack<std::string>::iterator ite = mstack.end();
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::cout << std::endl;
+
+		// Printing the top element
+		MutantStack<std::string>::iterator it_begin = mstack.begin();
+		std::cout << "original top: " << *it_begin << std::endl;
+
+		// Test if ++ work with iterator - printing the top element +1
+		++it_begin;
+		std::cout << "++ top: " << *it_begin << std::endl;
+
+		// Test if -- work with iterator - printing the top element -1
+		--it_begin;
+		std::cout << "-- top: " << *it_begin << std::endl;
+	}
+
 	return 0;
 }
+
